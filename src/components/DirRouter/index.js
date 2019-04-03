38acd8c423276ab1a99d74a6c9d7518336524145
moonlightming <605365071@qq.com>
2019-04-03 16:19:04 +0800

    logWindow输出插件修改为xterm.js'

--- src/components/DirRouter/index.js
+++ src/components/DirRouter/index.js
@@ -1,56 +1,55 @@
-import React, { Fragment } from 'react';
-import { Breadcrumb } from 'antd';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, {Fragment} from 'react';
+import {Breadcrumb} from 'antd';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import querystring from 'querystring';
 import BreadcrumbItem from 'antd/lib/breadcrumb/BreadcrumbItem';
 
 class DirRouter extends React.Component {
-
-    render() {
-        const { pathname, params } = this.props;
-        const param = querystring.parse(params.substring(1))
-        const dirList = param.dir.split('/').slice(1)
-        console.log(this.props)
-        return (
-            <Fragment>
-                <span style={{ marginRight: "5px" }} className="treeNodeUnselectable">{param.host} : /</span>
-                <Breadcrumb className="treeNodeUnselectable">
-                    {
-                        dirList.map((item, index) => {
-                            if (index === dirList.length - 1) {
-                                return <BreadcrumbItem key={index}>{item}</BreadcrumbItem>
-                            } else {
-                                const dir = '/' + dirList.slice(0, index + 1).join('/')
-                                return (
-                                    <BreadcrumbItem key={index}>
-                                        <Link
-                                            to={{
-                                                path: pathname,
-                                                search: querystring.stringify({
-                                                    host: param.host,
-                                                    dir,
-                                                    vmode: param.vmode
-                                                })
-                                            }}>
-                                            {item}
-                                        </Link>
-                                    </BreadcrumbItem>
-                                )
-                            }
-                        })
-                    }
-                </Breadcrumb>
-            </Fragment>
-        )
-    }
+  render () {
+    const {pathname, params} = this.props;
+    const param = querystring.parse (params.substring (1));
+    const dirList = param.dir.split ('/').slice (1);
+    return (
+      <Fragment>
+        <span style={{marginRight: '5px'}} className="treeNodeUnselectable">
+          {param.host} : /
+        </span>
+        <Breadcrumb className="treeNodeUnselectable">
+          {dirList.map ((item, index) => {
+            if (index === dirList.length - 1) {
+              return <BreadcrumbItem key={index}>{item}</BreadcrumbItem>;
+            } else {
+              const dir = '/' + dirList.slice (0, index + 1).join ('/');
+              return (
+                <BreadcrumbItem key={index}>
+                  <Link
+                    to={{
+                      path: pathname,
+                      search: querystring.stringify ({
+                        host: param.host,
+                        dir,
+                        vmode: param.vmode,
+                      }),
+                    }}
+                  >
+                    {item}
+                  </Link>
+                </BreadcrumbItem>
+              );
+            }
+          })}
+        </Breadcrumb>
+      </Fragment>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        pathname: state.getIn(['router', 'location', 'pathname']),
-        params: state.getIn(['router', 'location', 'search']),
-    }
-}
+const mapStateToProps = state => {
+  return {
+    pathname: state.getIn (['router', 'location', 'pathname']),
+    params: state.getIn (['router', 'location', 'search']),
+  };
+};
 
-export default connect(mapStateToProps, null)(DirRouter);
+export default connect (mapStateToProps, null) (DirRouter);
--- src/components/LogWindow/index.js
+++ src/components/LogWindow/index.js
@@ -1,90 +1,61 @@
 import React from 'react';
 import querystring from 'querystring';
 import {connect} from 'react-redux';
-import axios from 'axios';
-import {AutoSizer, List} from 'react-virtualized';
+import {Terminal} from 'xterm';
+import 'xterm/dist/xterm.css';
+import * as fit from 'xterm/lib/addons/fit/fit';
+import * as attach from 'xterm/lib/addons/attach/attach';
+import './index.css';
 
 class LogWindow extends React.Component {
   constructor (props) {
     super (props);
-    this.renderRow = this.renderRow.bind (this);
-    const logParams = querystring.stringify ({
-      host: props.params.host,
-      path: props.params.dir,
-      // password: 'vagrant',
-    });
+    Terminal.applyAddon (fit);
+    Terminal.applyAddon (attach);
+
     const location = window.location;
     // const protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://'
     // let socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + "/api/tailLog?" + logParams
-
+    const logParams = querystring.stringify ({
+      host: this.props.params.host,
+      path: this.props.params.dir,
+      // password: 'vagrant',
+    });
     let socketURL = 'ws://localhost:9090/api/tailLog?' + logParams;
     const ws = new WebSocket (socketURL);
-    ws.onopen = e => {
-      console.log ('Connected!', e);
-    };
 
-    ws.onmessage = e => {
-      console.log ('Received:', e.data);
-      this.setState ({
-        msgList: [...this.state.msgList, e.data],
-      });
-      // console.log(this.vList)
-      this.vList.scrollToRow (-1);
-    };
-
-    ws.onclose = e => {
-      console.log('Close')
-    }
+    let term = new Terminal ({
+      fontSize: 12,
+      cursorBlink: 5,
+      scrollback: 200,
+      tabStopWidth: 5,
+    });
 
     this.state = {
       ws,
-      msgList: [],
+      term,
     };
   }
 
-  getWebSocketAddr () {
-    axios.get ('/api/getWebSocketAddr').then (e => {
-      console.log (e);
-    });
+  componentDidMount () {
+    let {term, ws} = this.state;
+    term.open (document.getElementById ('xterm-win'));
+    term.attach (ws, false, true);
+    term.fit ();
   }
 
-  componentWillMount () {
-    console.log (this.props);
-    // this.getWebSocketAddr()
+  componentDidUpdate () {
+    let {term} = this.state;
+    term.fit ();
   }
 
   componentWillUnmount () {
-    console.log ('I am close');
+    this.state.term.detach(this.state.ws)
     this.state.ws.close ();
   }
 
-  renderRow({index, key, style}) {
-    // console.log(index, key, style)
-
-    return (
-      <div key={key} style={style}>
-        {this.state.msgList[index]}
-      </div>
-    );
-  }
-
   render () {
-    const {msgList} = this.state;
-    return (
-      <AutoSizer>
-        {({width, height}) => (
-          <List
-            ref={vList => (this.vList = vList)}
-            width={width}
-            height={height}
-            rowHeight={20}
-            rowRenderer={this.renderRow}
-            rowCount={msgList.length}
-            style={{whiteSpace: 'pre', outline: 'none'}}
-          />
-        )}
-      </AutoSizer>
-    );
+    return <div id="xterm-win" />;
   }
 }
 
