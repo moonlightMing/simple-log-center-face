ee594a83d36a9a7fc10e0fd5b259b6e9658f77e2
moonlightming <605365071@qq.com>
2019-04-11 01:10:52 +0800

    update

--- src/components/XtermWindow/index.js
+++ src/components/XtermWindow/index.js
@@ -1,7 +1,7 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import querystring from 'querystring';
-import {Terminal} from 'xterm';
-import {connect} from 'react-redux';
+import { Terminal } from 'xterm';
+import { connect } from 'react-redux';
 import 'xterm/dist/xterm.css';
 import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen';
 import * as fit from 'xterm/dist/addons/fit/fit';
@@ -9,27 +9,27 @@ import * as attach from 'xterm/lib/addons/attach/attach';
 import './index.css';
 
 class XtremWindow extends React.Component {
-  constructor (props) {
-    super (props);
-    Terminal.applyAddon (fit);
-    // Terminal.applyAddon (attach);
+  constructor(props) {
+    super(props);
+    Terminal.applyAddon(fit);
+    Terminal.applyAddon(attach);
     // Terminal.applyAddon (fullscreen);
 
-    const logParams = querystring.stringify ({
+    const logParams = querystring.stringify({
       host: this.props.params.host,
       path: this.props.params.dir,
       // password: 'vagrant',
     });
     let socketURL = 'ws://localhost:9090/api/terminalShell?' + logParams;
-    const ws = new WebSocket (socketURL);
+    const ws = new WebSocket(socketURL);
 
-    let term = new Terminal ({
-      cols: 100,
-      rows: 120,
+    let term = new Terminal({
+      // cols: 100,
+      // rows: 120,
       cursorBlink: 5,
       scrollback: 30,
       tabStopWidth: 4,
-      textarea: true,
+      textarea: false,
     });
 
     this.state = {
@@ -38,45 +38,44 @@ class XtremWindow extends React.Component {
     };
   }
 
-  componentDidMount () {
-    // Terminal.applyAddon (fit);
+  componentDidMount() {
     const { term, ws } = this.state;
-    term.open (document.getElementById ('terminal-container'));
-    // term.toggleFullScreen(true);
-    term.fit ();
+    term.open(document.getElementById('terminal-container'));
+    term.fit();
 
-    ws.onmessage = data => {
-      console.log (data);
-      term.write (data);
-    };
+    // ws.onmessage = data => {
+    //   console.log(data.data);
+    //   term.write(data.data);
+    // };
     ws.onclose = () => {
-      console.log ('close');
+      console.log('close');
+      term.writeln("The WebSocket Close...")
     };
-    // term.attach (ws, true, false);
+    term.attach (ws, true, false);
 
-    term.textarea.onkeydown = function (e) {
-      // console.log('User pressed key with keyCode: ', e.keyCode);
-      //console.log('编码',)
-      //ws.send(that.encodeBase64Content(e.keyCode.toString()));
-      //ws.send('bHM=');
-      term.write (e.keyCode);
-      ws.send (e.key);
-    };
+    // term.textarea.onkeydown = function (e) {
 
-    term.on ('data', function (data) {
-      console.log ('data xterm=>', data);
-      term.write (data);
+    //   console.log(e.keyCode.toString())
+    //   if (e.keyCode.toString() === "13") {
+    //     ws.send("\r\n")
+    //     // term.write("\r\n")
+    //   } else {
+    //     ws.send(e.key.toString());
+    //   }
+    // };
 
-      ws.send (data);
-    });
+    // term.on('data', function (data) {
+    //   // console.log('data xterm=>', data);
+    //   term.write(data.data);
+    // });
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.state.term.detach(this.state.ws)
     this.ws.close()
   }
 
-  render () {
+  render() {
     return (
       <Fragment>
         <div id="terminal-container" />
@@ -86,9 +85,9 @@ class XtremWindow extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  params: querystring.parse (
-    state.getIn (['router', 'location', 'search']).substring (1)
+  params: querystring.parse(
+    state.getIn(['router', 'location', 'search']).substring(1)
   ),
 });
 
-export default connect (mapStateToProps, null) (XtremWindow);
+export default connect(mapStateToProps, null)(XtremWindow);
